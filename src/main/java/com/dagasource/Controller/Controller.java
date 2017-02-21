886dc34b2105663d9f31d0e076357df4b8d54e2b
fmarcoccia <fabio.marcoccia@nexse.com>
2017-02-21 12:13:40 +0100

    Add bean config

--- src/main/java/com/dagasource/Controller/Controller.java
+++ src/main/java/com/dagasource/Controller/Controller.java
@@ -14,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -21,13 +22,13 @@ import org.springframework.web.bind.annotation.*;
  */
 
 @RestController
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Controller {
 
     private  BookRepository bookRepository;
     private  UserRepo userRepo;
 
+    @Autowired
     public Controller(BookRepository bookRepository, UserRepo userRepo) {
         this.bookRepository = bookRepository;
         this.userRepo = userRepo;
--- src/main/java/com/dagasource/InsertUser/UserRepo.java
+++ src/main/java/com/dagasource/InsertUser/UserRepo.java
@@ -1,5 +1,6 @@
 package com.dagasource.InsertUser;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
